1cf868d8afe1103f61a13752519e0b1f0e2f69ce
ebolite <moldehchez@gmail.com>
2023-02-09 11:23:33 -0600

    modified:   suburb_game/util.py     add server json utils

--- suburb_game/util.py
+++ suburb_game/util.py
@@ -1,9 +1,40 @@
 import os
+import json
 VERSION = "0.0.3"
 homedir = os.getcwd()
 subdirectories = next(os.walk("."))[1]
 if "suburb_game" in subdirectories: # if this is being run in vscode lol
     homedir += "\\suburb_game"
+
+def writejson(obj=None, fn=None):
+    if not os.path.exists(f"{homedir}\\json"):
+        os.makedirs(f"{homedir}\\json")
+        print(f"Created {homedir}\\json")
+    os.chdir(f"{homedir}\\json")
+    if fn != None:
+        with open(f"{fn}.json", "w") as f:
+            if obj == None:
+                obj = eval(f"{fn}")
+            if obj != None:
+                if obj != {} and obj != None:
+                    data = json.dump(obj, f, indent=4)
+                    f = data
+
+def readjson(obj, filename):
+    try:
+        os.chdir(f"{homedir}\\json")
+        with open(f"{filename}.json", "r") as f:
+            try:
+                data = json.load(f)
+            except json.JSONDecodeError:
+                print(f"UNABLE TO READ JSON {filename}")
+                writejson(obj, f"{homedir}\\json\\failed\\{filename}")
+                data = {}
+            return data
+    except FileNotFoundError:
+        print(f"File not found when reading json: '{filename}.json'. Overwriting with {obj}.")
+        writejson(obj, filename)
+        return obj
 
 def filter_item_name(name: str) -> str:
     return name.replace("+", " ")
