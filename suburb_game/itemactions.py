480a3476d8cfddb70fc962f8ec407da2a197ffc1
ebolite <moldehchez@gmail.com>
2023-02-17 13:08:37 -0600

    fix some use item prompts 	modified:   suburb_game/itemactions.py

--- suburb_game/itemactions.py
+++ suburb_game/itemactions.py
@@ -43,7 +43,7 @@ uncombine_card.use_prompt = "You uncombine the cards."
 
 insert_card = ItemAction("insert_card")
 insert_card.targeted = True
-insert_card.prompt = "Insert what into the {}?"
+insert_card.prompt = "Insert what into the {iname}?"
 insert_card.error_prompt = "Something is already inserted."
 insert_card.use_prompt = "You insert the card for {tname}."
 
@@ -64,13 +64,13 @@ cruxtrude.use_prompt = "A cruxite dowel flies out of the {iname}!"
 
 insert_dowel = ItemAction("insert_dowel")
 insert_dowel.targeted = True
-insert_dowel.prompt = "Insert what into the {}?"
+insert_dowel.prompt = "Insert what into the {iname}?"
 insert_dowel.error_prompt = "There's already a dowel inserted."
 insert_dowel.use_prompt = "You insert {tname} into the {iname}."
 
 insert_carved_dowel = ItemAction("insert_carved_dowel")
 insert_carved_dowel.targeted = True
-insert_carved_dowel.prompt = "Insert what into the {}?"
+insert_carved_dowel.prompt = "Insert what into the {iname}?"
 insert_carved_dowel.error_prompt = "There's already a dowel inserted."
 insert_carved_dowel.use_prompt = "You insert {tname} into the {iname}."
 
