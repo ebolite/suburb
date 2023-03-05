98797ac6e3a2f0785a616b2c39be4ac712fb77ca
ebolite <moldehchez@gmail.com>
2023-03-05 16:31:52 -0600

    add tooltip for assigning strife specibus 	modified:   suburb_game/suburb.py

--- suburb_game/suburb.py
+++ suburb_game/suburb.py
@@ -1061,6 +1061,12 @@ def strife_portfolio_scene(selected_kind:Optional[str]=None):
                 wield_button = render.TextButton(0.5, -0.15, 100, 30, ">wield", get_wield_button_func(instance_name), theme=theme)
                 wield_button.outline_color = theme.black
                 wield_button.bind_to(card_thumb)
+    else:
+        # selected kind is None
+        info_text = render.Text(0.5, 0.3, "Add an item to your strife portfolio by clicking the specibus icon while viewing it.")
+        info_text.color = theme.light
+        info_text.fontsize = 20
+        abstratus_icon = render.Image(0.5, 0.5, "sprites/itemdisplay/strife_card.png")
     back_button = render.Button(0.08, 0.95, "sprites/buttons/back.png", "sprites/buttons/backpressed.png", map_scene, theme=theme)
 
 @scene
