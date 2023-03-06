4d2ac915e05a9f99fa4096ca66235f1702f522ce
ebolite <moldehchez@gmail.com>
2023-03-06 15:37:01 -0600

    fix itemactions more 	modified:   suburb_game/itemactions.py

--- suburb_game/itemactions.py
+++ suburb_game/itemactions.py
@@ -21,23 +21,25 @@ class ItemAction():
     def use_func(self, instance: "sylladex.Instance") -> bool:
         return True
 
-    def prompt_message(self, item_name: Optional[str]):
+    def prompt_message(self, item_name: Optional[str]) -> str:
         if item_name is None: item_name = "MISSING ITEM"
         try: return self.prompt.format(iname_lower=item_name, iname=item_name.upper())
         except IndexError: return self.prompt
 
-    def error_message(self, item_name: Optional[str], target_name: Optional[str]=None):
+    def error_message(self, item_name: Optional[str], target_name: Optional[str]=None) -> str:
         if item_name is None: item_name = "MISSING ITEM"
+        if target_name is None: target_name = "MISSING ITEM"
         try:
             if target_name is None: 
-                self.error_prompt.format(iname_lower=item_name, iname=item_name.upper())
+                return self.error_prompt.format(iname_lower=item_name, iname=item_name.upper())
             else: return self.error_prompt.format(iname_lower=item_name, iname=item_name, tname_lower=target_name, tname=target_name.upper())
         except IndexError: return self.error_prompt
     
-    def use_message(self, item_name: Optional[str], target_name: Optional[str]=None):
+    def use_message(self, item_name: Optional[str], target_name: Optional[str]=None) -> str:
         if item_name is None: item_name = "MISSING ITEM"
+        if target_name is None: target_name = "MISSING ITEM"
         try:
-            if target_name is None: self.use_prompt.format(iname=item_name.upper())
+            if target_name is None: return self.use_prompt.format(iname=item_name.upper())
             else: return self.use_prompt.format(iname=item_name.upper(), tname=target_name.upper())
         except IndexError: return self.use_prompt
 
