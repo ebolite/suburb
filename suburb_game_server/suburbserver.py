9dbb709beabe5be4bb80bdc6c89a1194229f01ea
ebolite <moldehchez@gmail.com>
2023-03-18 18:03:44 -0500

    add unchain 	modified:   suburb_game_server/suburbserver.py

--- suburb_game_server/suburbserver.py
+++ suburb_game_server/suburbserver.py
@@ -487,6 +487,13 @@ def console_commands(player: sessions.Player, content: str):
             if target_name in util.players:
                 target = sessions.Player(target_name)
                 player.goto_room(target.room)
+        case "unchain":
+            if player.server_player is not None:
+                player.server_player_name = None
+                player.server_player.client_player_name = None
+            if player.client_player is not None:
+                player.client_player_name = None
+                player.client_player.server_player_name = None
         case "summon":
             target_name = " ".join(args)
             if target_name in util.players:
