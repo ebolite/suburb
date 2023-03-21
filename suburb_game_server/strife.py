b689d4776c53496f4d3de3433f5c62879d624c53
ebolite <moldehchez@gmail.com>
2023-03-20 19:41:51 -0500

    fix a bunch of crashes 	modified:   suburb_game_server/strife.py

--- suburb_game_server/strife.py
+++ suburb_game_server/strife.py
@@ -374,7 +374,15 @@ class Griefer():
             skill.use(self, targets_list)
 
     def get_random_submittable_skill(self) -> str:
-        usable_skills = [skill.name for skill in self.known_skills_list if skill.is_submittable_by(self)]
+        def is_usable_skill(skill: "skills.Skill"):
+            valid_targets = [self.strife.get_griefer(griefer_name) for griefer_name in skill.get_valid_targets(self)]
+            if skill.beneficial:
+                valid_targets = [griefer for griefer in valid_targets if griefer.team == self.team]
+            else:
+                valid_targets = [griefer for griefer in valid_targets if griefer.team == self.team]
+            if len(valid_targets) > 0: return True
+        usable_skills = [skill.name for skill in self.known_skills_list if skill.is_submittable_by(self) and is_usable_skill(skill)]
+        if "abscond" in usable_skills: usable_skills.remove("abscond")
         return random.choice(usable_skills)
 
     def submit_random_skill(self):
@@ -385,10 +393,9 @@ class Griefer():
             valid_targets = [griefer for griefer in valid_targets if griefer.team == self.team]
         else:
             valid_targets = [griefer for griefer in valid_targets if griefer.team != self.team]
-        try:
-            targets = random.choices(valid_targets, k=skill.num_targets)
-        except IndexError: 
-            return self.submit_random_skill()
+        targets = []
+        for i in range(skill.num_targets):
+            targets.append(random.choice(valid_targets))
         self.submit_skill(random_skill_name, [target.name for target in targets])
 
     def ai_use_skills(self):
