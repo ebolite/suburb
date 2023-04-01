55aa94d40ee4068e57bdf074ccfec39a19c5c050
ebolite <moldehchez@gmail.com>
2023-04-01 18:25:03 -0500

    1.6.0 	modified:   suburb_game/util.py

--- suburb_game/util.py
+++ suburb_game/util.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-VERSION = "v1.5.1-alpha"
+VERSION = "v1.6.0-alpha"
 homedir = os.getcwd()
 subdirectories = next(os.walk("."))[1]
 if "suburb_game" in subdirectories: # if this is being run in vscode lol
